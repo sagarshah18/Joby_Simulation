127744320d98eb2984a3774b5aa3317fd23c00a9
sagar <shah7sagar7@gmail.com>
2025-02-08 14:14:10 -0600

    adding more variables for handling calculations
    Also adding display function

--- main.cpp
+++ main.cpp
@@ -1,17 +1,44 @@
 #include "utility.hpp"
 
-vehicle::vehicle(const string nameFp, const double cruiseSpeedMphFp,
-                 const double batteryCapacityKwhFp,
-                 const double timeToChargeHrsFp,
-                 const double energyUseAtCruiseKwhMileFp,
-                 const uint32_t passengerCountFp,
-                 const double probabilityOfFaultPerHrFp)
+vehicleType::vehicleType(const string nameFp, const double cruiseSpeedMphFp,
+                         const double batteryCapacityKwhFp,
+                         const double timeToChargeHrsFp,
+                         const double energyUseAtCruiseKwhMileFp,
+                         const uint32_t passengerCountFp,
+                         const double probabilityOfFaultPerHrFp)
     : name(nameFp), cruiseSpeedMph(cruiseSpeedMphFp),
       batteryCapacityKwh(batteryCapacityKwhFp),
       timeToChargeHrs(timeToChargeHrsFp),
       energyUseAtCruiseKwhMile(energyUseAtCruiseKwhMileFp),
       passengerCount(passengerCountFp),
-      probabilityOfFaultPerHr(probabilityOfFaultPerHrFp) {}
+      probabilityOfFaultPerHr(probabilityOfFaultPerHrFp),
+      singleChargeMaxRangeMiles(batteryCapacityKwh / energyUseAtCruiseKwhMile),
+      singleChargeMaxFlightTimeInHrs(singleChargeMaxRangeMiles /
+                                     cruiseSpeedMph),
+      cruisePowerKw(batteryCapacityKwh / singleChargeMaxFlightTimeInHrs){};
+
+void vehicleType::display() const {
+  cout << "Given parameters:\n";
+  cout << "Company Name: " << name << "\n";
+  cout << "Cruise Speed (mph): " << cruiseSpeedMph << "\n";
+  cout << "Battery Capacity (kWh): " << batteryCapacityKwh << "\n";
+  cout << "Time to Charge (hours): " << timeToChargeHrs << "\n";
+  cout << "Energy use at Cruise (kWh/mile): " << energyUseAtCruiseKwhMile
+       << "\n";
+  cout << "Passenger Count: " << passengerCount << "\n";
+  cout << "Probability of fault per hour: " << probabilityOfFaultPerHr << "\n";
+  cout << "Hidden parameters:\n";
+  cout << "Max Range in Single Charge (miles): " << singleChargeMaxRangeMiles
+       << "\n";
+  cout << "Max Flight time in Single Charge (hours): "
+       << singleChargeMaxFlightTimeInHrs << "\n";
+  cout << "Cruise Power (kW): " << cruisePowerKw << "\n";
+};
+
+vehicle::vehicle(vehicleType *vTypeFp) : vType(vTypeFp) {
+  // starting with full capacity
+  data.currentBatteryChargeInKwh = vType->batteryCapacityKwh;
+}
 
 int main() {
   cout << "Dummy\n";
--- utility.hpp
+++ utility.hpp
@@ -3,6 +3,35 @@
 
 using namespace std;
 
+const uint32_t MAX_NUMBER_OF_VEHICLE = 20;
+const uint32_t MAX_NUMBER_OF_CHARGERS = 3;
+const double TOTAL_SIMULATION_IN_HRS = 3.0;
+const double SIMULATION_STEP_SIZE_IN_HRS = 1.0 / 3600.0;
+
+// Vehicle state machine
+enum class vehicleState {
+  FLYING = 0,
+  IN_QUEUE_FOR_CHARGER,
+  CHARGING,
+};
+
+struct totalVehicleData {
+  double chargeTimeHrs = 0;
+  double timeInWaitHrs = 0;
+  uint32_t numberOfCharges = 0;
+  uint32_t numberOfWaitings = 0;
+  double flightDurationHrs = 0;
+  uint32_t numberOfFlights = 0;
+  double distanceInMiles = 0;
+  uint32_t totalFaultsOccured = 0;
+
+  vehicleState currentState = vehicleState::FLYING;
+  vehicleState lastState = vehicleState::CHARGING;
+
+  double currentBatteryChargeInKwh;
+};
+
+// Individual structures used for each vehicle
 struct individualVehicleData {
   // Given tracking for each vehicle type
   double avgFlightTimePerFlight = 0;
@@ -10,9 +39,19 @@ struct individualVehicleData {
   double avgTimeChargingPerChargeSession = 0;
   uint32_t totalNumberOfFaults = 0;
   double totalNumberOFPassengerMiles = 0;
+
+  // supporting variables
+  double totalChargeTimeHrs = 0;
+  double totalTimeInWaitHrs = 0;
+  uint32_t totalNumberOfCharges = 0;
+  uint32_t totalNumberOfWaitings = 0;
+  double totalFlightDurationHrs = 0;
+  uint32_t totalNumberOfFlights = 0;
+  double totalDistanceInMiles = 0;
+  uint32_t totalVehicleUsedInPool = 0;
 };
 
-struct vehicle {
+struct vehicleType {
   // Given parameters
   const string name;
   const double cruiseSpeedMph;
@@ -24,9 +63,22 @@ struct vehicle {
 
   individualVehicleData vehicleData;
 
-  vehicle(const string nameFp, const double cruiseSpeedMphFp,
-          const double batteryCapacityKwhFp, const double timeToChargeHrsFp,
-          const double energyUseAtCruiseKwhMileFp,
-          const uint32_t passengerCountFp,
-          const double probabilityOfFaultPerHrFp);
+  vehicleType(const string nameFp, const double cruiseSpeedMphFp,
+              const double batteryCapacityKwhFp, const double timeToChargeHrsFp,
+              const double energyUseAtCruiseKwhMileFp,
+              const uint32_t passengerCountFp,
+              const double probabilityOfFaultPerHrFp);
+
+  // Hidden parameters
+  const double singleChargeMaxRangeMiles;
+  const double singleChargeMaxFlightTimeInHrs;
+  const double cruisePowerKw;
+
+  void display() const;
+};
+
+struct vehicle {
+  vehicle(vehicleType *vTypeFp);
+  vehicleType *vType;
+  totalVehicleData data;
 };
